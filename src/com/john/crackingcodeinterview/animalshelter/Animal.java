4951f1052ac06a97a4020f8e92d8976c310eafb3
John Borries <pippentook@gmail.com>
2015-06-24 23:38:49 -0700

    had to change the package for some reason

--- src/com/john/crackingcodeinterview/animalshelter/Animal.java
+++ src/com/john/crackingcodeinterview/animalshelter/Animal.java
@@ -1,14 +1,14 @@
-package com.john.animalshelter;
+package com.john.crackingcodeinterview.animalshelter;
 
 public class Animal {
-    private final AnimalType animalType;
+	private final AnimalType animalType;
 
-    public Animal(AnimalType animalType) {
-        this.animalType = animalType;
-    }
+	public Animal(AnimalType animalType) {
+		this.animalType = animalType;
+	}
 
-    public AnimalType getAnimalType() {
-        return animalType;
-    }
+	public AnimalType getAnimalType() {
+		return animalType;
+	}
 
 }
--- src/com/john/crackingcodeinterview/animalshelter/AnimalShelter.java
+++ src/com/john/crackingcodeinterview/animalshelter/AnimalShelter.java
@@ -1,4 +1,4 @@
-package com.john.animalshelter;
+package com.john.crackingcodeinterview.animalshelter;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -8,78 +8,81 @@ import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class AnimalShelter {
-    Map<AnimalType, Queue<AnimalEntry>> animals = new HashMap<AnimalType, Queue<AnimalEntry>>();
+	Map<AnimalType, Queue<AnimalEntry>> animals = new HashMap<AnimalType, Queue<AnimalEntry>>();
 
-    public AnimalShelter(AnimalType... supportedAnimalTypes) {
-        for (AnimalType animalType : supportedAnimalTypes) {
-            animals.put(animalType, new LinkedList<AnimalEntry>());
-        }
-    }
+	public AnimalShelter(AnimalType... supportedAnimalTypes) {
+		for (AnimalType animalType : supportedAnimalTypes) {
+			animals.put(animalType, new LinkedList<AnimalEntry>());
+		}
+	}
 
-    public Animal dequeueAny() {
-        List<Queue<AnimalEntry>> queuesWithAnimals = findQueuesWithAnimals();
-        if (queuesWithAnimals.isEmpty()) {
-            throw new NoSuchElementException("no animals!");
-        }
+	public Animal dequeueAny() {
+		List<Queue<AnimalEntry>> queuesWithAnimals = findQueuesWithAnimals();
+		if (queuesWithAnimals.isEmpty()) {
+			throw new NoSuchElementException("no animals!");
+		}
 
-        Queue<AnimalEntry> queueWithOldestAnimal = findQueueWithOldestAnimal(queuesWithAnimals);
-        return queueWithOldestAnimal.remove().getAnimal();
-    }
+		Queue<AnimalEntry> queueWithOldestAnimal = findQueueWithOldestAnimal(queuesWithAnimals);
+		return queueWithOldestAnimal.remove().getAnimal();
+	}
 
-    private List<Queue<AnimalEntry>> findQueuesWithAnimals() {
-        List<Queue<AnimalEntry>> queuesWithAnimals = new LinkedList<Queue<AnimalEntry>>();
-        for (Queue<AnimalEntry> animalQueue : animals.values()) {
-            if (!animalQueue.isEmpty()) {
-                queuesWithAnimals.add(animalQueue);
-            }
-        }
-        return queuesWithAnimals;
-    }
+	private List<Queue<AnimalEntry>> findQueuesWithAnimals() {
+		List<Queue<AnimalEntry>> queuesWithAnimals = new LinkedList<Queue<AnimalEntry>>();
+		for (Queue<AnimalEntry> animalQueue : animals.values()) {
+			if (!animalQueue.isEmpty()) {
+				queuesWithAnimals.add(animalQueue);
+			}
+		}
+		return queuesWithAnimals;
+	}
 
-    private Queue<AnimalEntry> findQueueWithOldestAnimal(List<Queue<AnimalEntry>> queuesWithAnimals) {
-        Queue<AnimalEntry> queueWithOldestAnimal = queuesWithAnimals.get(0);
-        for (Queue<AnimalEntry> animalQueue : queuesWithAnimals) {
-            if (animalQueue.peek().getInsertedEpochTime() > queueWithOldestAnimal.peek().getInsertedEpochTime()) {
-                queueWithOldestAnimal = animalQueue;
-            }
-        }
+	private Queue<AnimalEntry> findQueueWithOldestAnimal(
+			List<Queue<AnimalEntry>> queuesWithAnimals) {
+		Queue<AnimalEntry> queueWithOldestAnimal = queuesWithAnimals.get(0);
+		for (Queue<AnimalEntry> animalQueue : queuesWithAnimals) {
+			if (animalQueue.peek().getInsertedEpochTime() > queueWithOldestAnimal
+					.peek().getInsertedEpochTime()) {
+				queueWithOldestAnimal = animalQueue;
+			}
+		}
 
-        return queueWithOldestAnimal;
-    }
+		return queueWithOldestAnimal;
+	}
 
-    public Animal dequeueCat() {
-        return dequeueAnimal(AnimalType.CAT);
-    }
+	public Animal dequeueCat() {
+		return dequeueAnimal(AnimalType.CAT);
+	}
 
-    public Animal dequeueDog() {
-        return dequeueAnimal(AnimalType.DOG);
-    }
+	public Animal dequeueDog() {
+		return dequeueAnimal(AnimalType.DOG);
+	}
 
-    public Animal dequeueAnimal(AnimalType animalType) {
-        verifyAnimalTypeSupported(animalType);
+	public Animal dequeueAnimal(AnimalType animalType) {
+		verifyAnimalTypeSupported(animalType);
 
-        return dequeueAnimal(animals.get(animalType));
-    }
+		return dequeueAnimal(animals.get(animalType));
+	}
 
-    private Animal dequeueAnimal(Queue<AnimalEntry> animalQueue) {
-        if (animalQueue.isEmpty()) {
-            throw new NoSuchElementException("no animal here");
-        }
+	private Animal dequeueAnimal(Queue<AnimalEntry> animalQueue) {
+		if (animalQueue.isEmpty()) {
+			throw new NoSuchElementException("no animal here");
+		}
 
-        return animalQueue.remove().getAnimal();
-    }
+		return animalQueue.remove().getAnimal();
+	}
 
-    public void enqueueAnimal(Animal animal) {
-        verifyAnimalTypeSupported(animal.getAnimalType());
+	public void enqueueAnimal(Animal animal) {
+		verifyAnimalTypeSupported(animal.getAnimalType());
 
-        AnimalEntry entry = new AnimalEntry(animal, System.currentTimeMillis());
-        animals.get(animal.getAnimalType()).add(entry);
-    }
+		AnimalEntry entry = new AnimalEntry(animal, System.currentTimeMillis());
+		animals.get(animal.getAnimalType()).add(entry);
+	}
 
-    private void verifyAnimalTypeSupported(AnimalType animalType) {
-        if (!animals.containsKey(animalType)) {
-            throw new NoSuchElementException("animal type not supported:" + animalType);
-        }
-    }
+	private void verifyAnimalTypeSupported(AnimalType animalType) {
+		if (!animals.containsKey(animalType)) {
+			throw new NoSuchElementException("animal type not supported:"
+					+ animalType);
+		}
+	}
 
 }
