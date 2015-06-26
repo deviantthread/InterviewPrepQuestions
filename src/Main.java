507f41e5b8d674324db914877018a53281e59268
unknown <borriesj@SEA-6900074453.ant.amazon.com>
2015-06-26 15:28:44 -0700

    test commit from windows

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-import java.net.HttpURLConnection;
-
-import org.apache.commons.io.IOUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("1").append("2");
-        String s1 = sb.toString();
-        sb.append("3");
-        String s2 = sb.toString();
-        System.out.println(s1 + " : " + s2);
-    }
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("1").append("2");
+		String s1 = sb.toString();
+		sb.append("3");
+		String s2 = sb.toString();
+		System.out.println(s1 + " : " + s2);
+	}
 
 }
