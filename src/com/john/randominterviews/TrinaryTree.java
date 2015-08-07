ac300334129c00d096672d07b3fc85307b2ad341
Borries <borriesj@amazon.com>
2015-08-07 18:58:46 +0000

    think it's cleaned up so it'll work, need to run unit tests

--- src/com/john/randominterviews/TrinaryTree.java
+++ src/com/john/randominterviews/TrinaryTree.java
@@ -2,192 +2,167 @@ package com.john.randominterviews;
 
 // source file
 
-import com.google.common.annotations.VisibleForTesting;
-
-// add guava to your build path for @VisibleForTesting 
-// http://search.maven.org/remotecontent?filepath=com/google/guava/guava/18.0/guava-18.0.jar
 public class TrinaryTree {
 
-	private Node root;
+    private Node root;
 
-	public void insert(int val) {
-		if (root == null) {
-			root = new Node(val);
-		} else {
-			insertRecurse(root, val);
-		}
-	}
+    // package private for unit tests
+    Node getRoot() {
+        return root;
+    }
 
-	private void insertRecurse(Node node, int valToAdd) {
-		if (node.val < valToAdd) {
-			if (node.right != null) {
-				insertRecurse(node.right, valToAdd);
-			} else {
-				node.right = new Node(valToAdd);
-			}
-		} else if (node.val > valToAdd) {
-			if (node.left != null) {
-				insertRecurse(node.left, valToAdd);
-			} else {
-				node.left = new Node(valToAdd);
-			}
-		} else {
-			Node oldMidHead = node.mid;
-			Node newMidHead = new Node(valToAdd);
-			newMidHead.mid = oldMidHead;
-			node.mid = newMidHead;
-		}
-	}
+    public void insert(int val) {
+        if (root == null) {
+            root = new Node(val);
+        } else {
+            insertRecurse(root, val);
+        }
+    }
 
-	public void delete(int val) {
-		ParentDeleteNodePair pair = findParent(root, val);
+    private void insertRecurse(Node node, int valToAdd) {
+        if (node.val < valToAdd) {
+            if (node.right != null) {
+                insertRecurse(node.right, valToAdd);
+            } else {
+                node.right = new Node(valToAdd);
+            }
+        } else if (node.val > valToAdd) {
+            if (node.left != null) {
+                insertRecurse(node.left, valToAdd);
+            } else {
+                node.left = new Node(valToAdd);
+            }
+        } else {
+            Node oldMidHead = node.mid;
+            Node newMidHead = new Node(valToAdd);
+            newMidHead.mid = oldMidHead;
+            node.mid = newMidHead;
+        }
+    }
 
-		boolean valExistsInTree = pair != null;
-		if (valExistsInTree) {
-			deleteNode(pair, true);
-		}
-	}
+    public void delete(int val) {
+        ParentDeleteNodePair pair = findParent(root, val);
 
-	private void deleteNode(ParentDeleteNodePair pair, boolean deleteMidIfPossible) {
-		Node node = pair.nodeToDelete;
-		boolean hasRight = node.right != null;
-		boolean hasLeft = node.left != null;
-		boolean isLeaf = !(hasRight || hasLeft);
-		boolean hasExactlyOneChild = hasRight ^ hasLeft;
+        boolean valExistsInTree = pair != null;
+        if (valExistsInTree) {
+            deleteNode(pair, true);
+        }
+    }
 
-		if (node.mid != null && deleteMidIfPossible) {
-			deleteMid(node);
-		} else if (isLeaf) {
-			deleteNoChild(pair);
-		} else if (hasExactlyOneChild) {
-			deleteOneChild(pair);
-		} else {
-			deleteTwoChildren(pair);
-		}
-	}
+    private void deleteNode(ParentDeleteNodePair pair, boolean deleteMidIfPossible) {
+        Node node = pair.nodeToDelete;
+        boolean hasRight = node.right != null;
+        boolean hasLeft = node.left != null;
+        boolean isLeaf = !(hasRight || hasLeft);
+        boolean hasExactlyOneChild = hasRight ^ hasLeft;
 
-	private void deleteMid(Node node) {
-		node.mid = node.mid.mid;
-	}
+        if (node.mid != null && deleteMidIfPossible) {
+            deleteMid(node);
+        } else if (isLeaf) {
+            deleteNoChild(pair);
+        } else if (hasExactlyOneChild) {
+            deleteOneChild(pair);
+        } else {
+            deleteTwoChildren(pair);
+        }
+    }
 
-	private void deleteNoChild(ParentDeleteNodePair pair) {
-		Node parent = pair.parent;
+    private void deleteMid(Node node) {
+        node.mid = node.mid.mid;
+    }
 
-		if (pair.isRoot()) {
-			root = null;
-		} else if (parent.right == pair.nodeToDelete) {
-			parent.right = null;
-		} else {
-			parent.left = null;
-		}
-	}
+    private void deleteNoChild(ParentDeleteNodePair pair) {
+        Node parent = pair.parent;
 
-	private void deleteOneChild(ParentDeleteNodePair pair) {
-		Node parent = pair.parent;
-		Node nodeToDelete = pair.nodeToDelete;
-		Node child = nodeToDelete.right != null ? nodeToDelete.right : nodeToDelete.left;
+        if (pair.isRoot()) {
+            root = null;
+        } else if (parent.right == pair.nodeToDelete) {
+            parent.right = null;
+        } else {
+            parent.left = null;
+        }
+    }
 
-		if (pair.isRoot()) {
-			root = child;
-		} else if (parent.right == pair.nodeToDelete) {
-			parent.right = child;
-		} else {
-			parent.left = child;
-		}
+    private void deleteOneChild(ParentDeleteNodePair pair) {
+        Node parent = pair.parent;
+        Node nodeToDelete = pair.nodeToDelete;
+        Node child = nodeToDelete.right != null ? nodeToDelete.right : nodeToDelete.left;
 
-	}
+        if (pair.isRoot()) {
+            root = child;
+        } else if (parent.right == pair.nodeToDelete) {
+            parent.right = child;
+        } else {
+            parent.left = child;
+        }
 
-	private void deleteTwoChildren(ParentDeleteNodePair pair) {
-		ParentDeleteNodePair nextLargestPair = findNextLargest(pair.nodeToDelete);
-		deleteNode(nextLargestPair, false);
-		Node node = pair.nodeToDelete;
-		Node replacementNode = nextLargestPair.nodeToDelete;
+    }
 
-		node.val = replacementNode.val;
-		node.mid = replacementNode.mid;
-	}
+    private void deleteTwoChildren(ParentDeleteNodePair pair) {
+        ParentDeleteNodePair nextLargestPair = findNextLargest(pair.nodeToDelete);
+        deleteNode(nextLargestPair, false);
+        Node node = pair.nodeToDelete;
+        Node replacementNode = nextLargestPair.nodeToDelete;
 
-	private ParentDeleteNodePair findNextLargest(Node node) {
-		Node parent = node;
-		Node curr = node.right;
+        node.val = replacementNode.val;
+        node.mid = replacementNode.mid;
+    }
 
-		while (curr.left != null) {
-			parent = curr;
-			curr = curr.left;
-		}
-		return new ParentDeleteNodePair(parent, curr);
-	}
+    private ParentDeleteNodePair findNextLargest(Node node) {
+        Node parent = node;
+        Node curr = node.right;
 
-	private ParentDeleteNodePair findParent(Node node, int val) {
-		if (node == null) {
-			return null;
-		}
+        while (curr.left != null) {
+            parent = curr;
+            curr = curr.left;
+        }
+        return new ParentDeleteNodePair(parent, curr);
+    }
 
-		if (node.left != null && node.left.val == val) {
-			return new ParentDeleteNodePair(node, node.left);
-		} else if (node.right != null && node.right.val == val) {
-			return new ParentDeleteNodePair(node, node.right);
-		}
+    private ParentDeleteNodePair findParent(Node node, int val) {
+        if (node == null) {
+            return null;
+        }
 
-		if (node.val < val) {
-			return findParent(node.right, val);
-		} else if (node.val > val) {
-			return findParent(node.left, val);
-		} else {
-			return new ParentDeleteNodePair(null, node);
-		}
+        if (node.left != null && node.left.val == val) {
+            return new ParentDeleteNodePair(node, node.left);
+        } else if (node.right != null && node.right.val == val) {
+            return new ParentDeleteNodePair(node, node.right);
+        }
 
-	}
+        if (node.val < val) {
+            return findParent(node.right, val);
+        } else if (node.val > val) {
+            return findParent(node.left, val);
+        } else {
+            return new ParentDeleteNodePair(null, node);
+        }
 
-	@VisibleForTesting
-	Node getRoot() {
-		return this.root;
-	}
+    }
 
-	@VisibleForTesting
-	static class Node {
-		private int val;
-		private Node left = null;
-		private Node right = null;
-		private Node mid = null;
+    static class Node {
+        int val;
+        Node left = null;
+        Node right = null;
+        Node mid = null;
 
-		public Node(int val) {
-			this.val = val;
-		}
+        public Node(int val) {
+            this.val = val;
+        }
 
-		@VisibleForTesting
-		int getVal() {
-			return val;
-		}
+    }
 
-		@VisibleForTesting
-		Node getLeft() {
-			return left;
-		}
+    private static class ParentDeleteNodePair {
+        private final Node parent;
+        private final Node nodeToDelete;
 
-		@VisibleForTesting
-		Node getRight() {
-			return right;
-		}
+        public ParentDeleteNodePair(Node parent, Node nodeToDelete) {
+            this.parent = parent;
+            this.nodeToDelete = nodeToDelete;
+        }
 
-		@VisibleForTesting
-		Node getMid() {
-			return mid;
-		}
-
-	}
-
-	private static class ParentDeleteNodePair {
-		private Node parent;
-		private Node nodeToDelete;
-
-		public ParentDeleteNodePair(Node parent, Node nodeToDelete) {
-			this.parent = parent;
-			this.nodeToDelete = nodeToDelete;
-		}
-
-		public boolean isRoot() {
-			return parent == null;
-		}
-	}
+        public boolean isRoot() {
+            return parent == null;
+        }
+    }
 }
--- tst/com/john/randominterviews/TrinaryTreeTest.java
+++ tst/com/john/randominterviews/TrinaryTreeTest.java
@@ -14,227 +14,224 @@ import com.john.randominterviews.TrinaryTree.Node;
 //https://hamcrest.googlecode.com/files/hamcrest-all-1.3.jar
 public class TrinaryTreeTest {
 
-	private TrinaryTree classUnderTest = null;
+    private TrinaryTree classUnderTest = null;
 
-	@Before
-	public void setup() {
-		// reset to a fresh tree for each test
-		classUnderTest = new TrinaryTree();
-	}
+    @Before
+    public void setup() {
+        // reset to a fresh tree before each test
+        classUnderTest = new TrinaryTree();
+    }
 
-	@After
-	public void isValidTrinaryTree() {
-		// verify at the end of every test, the tree is in a valid state
-		Assert.assertTrue(isValidTrinaryTree(classUnderTest.getRoot(), Integer.MIN_VALUE, Integer.MAX_VALUE));
-	}
+    @After
+    public void isValidTrinaryTree() {
+        // verify at the end of every test, the tree is in a valid state
+        Assert.assertTrue(isValidTrinaryTree(classUnderTest.getRoot(), Integer.MIN_VALUE, Integer.MAX_VALUE));
+    }
 
-	@Test
-	public void testInsertAllUnique() {
-		classUnderTest.insert(5);
-		classUnderTest.insert(15);
-		classUnderTest.insert(3);
-		classUnderTest.insert(7);
+    @Test
+    public void testInsertAllUnique() {
+        classUnderTest.insert(5);
+        classUnderTest.insert(15);
+        classUnderTest.insert(3);
+        classUnderTest.insert(7);
 
-		Assert.assertEquals("3,5,7,15,", convertTreeToString());
-	}
+        Assert.assertEquals("3,5,7,15,", convertTreeToString());
+    }
 
-	@Test
-	public void testInsertMid() {
-		classUnderTest.insert(5);
-		classUnderTest.insert(3);
-		classUnderTest.insert(7);
-		classUnderTest.insert(5);
+    @Test
+    public void testInsertMid() {
+        classUnderTest.insert(5);
+        classUnderTest.insert(3);
+        classUnderTest.insert(7);
+        classUnderTest.insert(5);
 
-		Assert.assertEquals("3,5,5,7,", convertTreeToString());
-	}
+        Assert.assertEquals("3,5,5,7,", convertTreeToString());
+    }
 
-	@Test
-	public void testDeleteMid() {
-		classUnderTest.insert(1);
-		classUnderTest.insert(3);
-		classUnderTest.insert(3);
-		classUnderTest.insert(4);
-		Assert.assertEquals("1,3,3,4,", convertTreeToString());
+    @Test
+    public void testDeleteMid() {
+        classUnderTest.insert(1);
+        classUnderTest.insert(3);
+        classUnderTest.insert(3);
+        classUnderTest.insert(4);
+        Assert.assertEquals("1,3,3,4,", convertTreeToString());
 
-		classUnderTest.delete(3);
+        classUnderTest.delete(3);
 
-		Assert.assertEquals("1,3,4,", convertTreeToString());
-	}
+        Assert.assertEquals("1,3,4,", convertTreeToString());
+    }
 
-	@Test
-	public void testDeleteLeaf() {
-		classUnderTest.insert(1);
-		classUnderTest.insert(2);
-		classUnderTest.insert(3);
-		classUnderTest.insert(4);
-		Assert.assertEquals("1,2,3,4,", convertTreeToString());
+    @Test
+    public void testDeleteLeaf() {
+        classUnderTest.insert(1);
+        classUnderTest.insert(2);
+        classUnderTest.insert(3);
+        classUnderTest.insert(4);
+        Assert.assertEquals("1,2,3,4,", convertTreeToString());
 
-		classUnderTest.delete(4);
+        classUnderTest.delete(4);
 
-		Assert.assertEquals("1,2,3,", convertTreeToString());
-	}
+        Assert.assertEquals("1,2,3,", convertTreeToString());
+    }
 
-	@Test
-	public void testDeleteOneChild() {
-		classUnderTest.insert(1);
-		classUnderTest.insert(2);
-		classUnderTest.insert(3);
-		classUnderTest.insert(4);
-		Assert.assertEquals("1,2,3,4,", convertTreeToString());
+    @Test
+    public void testDeleteOneChild() {
+        classUnderTest.insert(1);
+        classUnderTest.insert(2);
+        classUnderTest.insert(3);
+        classUnderTest.insert(4);
+        Assert.assertEquals("1,2,3,4,", convertTreeToString());
 
-		classUnderTest.delete(3);
+        classUnderTest.delete(3);
 
-		Assert.assertEquals("1,2,4,", convertTreeToString());
-	}
+        Assert.assertEquals("1,2,4,", convertTreeToString());
+    }
 
-	@Test
-	public void testDeleteTwoChildren() {
-		classUnderTest.insert(10);
-		classUnderTest.insert(5);
-		classUnderTest.insert(7);
-		classUnderTest.insert(7);
-		classUnderTest.insert(1);
-		classUnderTest.insert(7);
-		Assert.assertEquals("1,5,7,7,7,10,", convertTreeToString());
+    @Test
+    public void testDeleteTwoChildren() {
+        classUnderTest.insert(10);
+        classUnderTest.insert(5);
+        classUnderTest.insert(7);
+        classUnderTest.insert(7);
+        classUnderTest.insert(1);
+        classUnderTest.insert(7);
+        Assert.assertEquals("1,5,7,7,7,10,", convertTreeToString());
 
-		classUnderTest.delete(5);
+        classUnderTest.delete(5);
 
-		Assert.assertEquals("1,7,7,7,10,", convertTreeToString());
-	}
+        Assert.assertEquals("1,7,7,7,10,", convertTreeToString());
+    }
 
-	// write 'is still binary tree principle held' method and add it in an
-	// @After method
+    @Test
+    public void testDeleteTwoChildrenBigRightTree() {
+        classUnderTest.insert(100);
+        classUnderTest.insert(50);
+        classUnderTest.insert(70);
+        classUnderTest.insert(10);
+        // 10 < 50 < big tree < 70
+        classUnderTest.insert(60);
+        classUnderTest.insert(56);
+        classUnderTest.insert(58);
+        classUnderTest.insert(68);
+        classUnderTest.insert(51);
 
-	@Test
-	public void testDeleteTwoChildrenBigRightTree() {
-		classUnderTest.insert(100);
-		classUnderTest.insert(50);
-		classUnderTest.insert(70);
-		classUnderTest.insert(10);
-		// 10 < 50 < big tree < 70
-		classUnderTest.insert(60);
-		classUnderTest.insert(56);
-		classUnderTest.insert(58);
-		classUnderTest.insert(68);
-		classUnderTest.insert(51);
+        Assert.assertEquals("10,50,51,56,58,60,68,70,100,", convertTreeToString());
 
-		Assert.assertEquals("10,50,51,56,58,60,68,70,100,", convertTreeToString());
+        classUnderTest.delete(50);
 
-		classUnderTest.delete(50);
+        Assert.assertEquals("10,51,56,58,60,68,70,100,", convertTreeToString());
+    }
 
-		Assert.assertEquals("10,51,56,58,60,68,70,100,", convertTreeToString());
-	}
+    @Test
+    public void testDeleteRootNoChild() {
+        classUnderTest.insert(10);
+        Assert.assertEquals("10,", convertTreeToString());
 
-	@Test
-	public void testDeleteRootNoChild() {
-		classUnderTest.insert(10);
-		Assert.assertEquals("10,", convertTreeToString());
+        classUnderTest.delete(10);
 
-		classUnderTest.delete(10);
+        Assert.assertEquals("", convertTreeToString());
+    }
 
-		Assert.assertEquals("", convertTreeToString());
-	}
+    @Test
+    public void testDeleteRootOneChild() {
+        classUnderTest.insert(10);
+        classUnderTest.insert(5);
+        Assert.assertEquals("5,10,", convertTreeToString());
 
-	@Test
-	public void testDeleteRootOneChild() {
-		classUnderTest.insert(10);
-		classUnderTest.insert(5);
-		Assert.assertEquals("5,10,", convertTreeToString());
+        classUnderTest.delete(10);
 
-		classUnderTest.delete(10);
+        Assert.assertEquals("5,", convertTreeToString());
+    }
 
-		Assert.assertEquals("5,", convertTreeToString());
-	}
+    @Test
+    public void testDeleteRootTwoChildren() {
+        classUnderTest.insert(10);
+        classUnderTest.insert(5);
+        classUnderTest.insert(1);
+        classUnderTest.insert(7);
+        Assert.assertEquals("1,5,7,10,", convertTreeToString());
 
-	@Test
-	public void testDeleteRootTwoChildren() {
-		classUnderTest.insert(10);
-		classUnderTest.insert(5);
-		classUnderTest.insert(1);
-		classUnderTest.insert(7);
-		Assert.assertEquals("1,5,7,10,", convertTreeToString());
+        classUnderTest.delete(10);
 
-		classUnderTest.delete(10);
+        Assert.assertEquals("1,5,7,", convertTreeToString());
+    }
 
-		Assert.assertEquals("1,5,7,", convertTreeToString());
-	}
+    @Test
+    public void testDeleteNonExistent() {
+        classUnderTest.insert(1);
+        classUnderTest.insert(2);
+        classUnderTest.insert(3);
+        classUnderTest.insert(4);
+        Assert.assertEquals("1,2,3,4,", convertTreeToString());
 
-	@Test
-	public void testDeleteNonExistent() {
-		classUnderTest.insert(1);
-		classUnderTest.insert(2);
-		classUnderTest.insert(3);
-		classUnderTest.insert(4);
-		Assert.assertEquals("1,2,3,4,", convertTreeToString());
+        classUnderTest.delete(500);
 
-		classUnderTest.delete(500);
+        Assert.assertEquals("1,2,3,4,", convertTreeToString());
+    }
 
-		Assert.assertEquals("1,2,3,4,", convertTreeToString());
-	}
+    @Test
+    public void testDeleteBeforeInsert() {
+        Assert.assertEquals("", convertTreeToString());
 
-	@Test
-	public void testDeleteBeforeInsert() {
-		Assert.assertEquals("", convertTreeToString());
+        classUnderTest.delete(3);
 
-		classUnderTest.delete(3);
+        Assert.assertEquals("", convertTreeToString());
+        classUnderTest.insert(5);
+        Assert.assertEquals("5,", convertTreeToString());
+    }
 
-		Assert.assertEquals("", convertTreeToString());
-		classUnderTest.insert(5);
-		Assert.assertEquals("5,", convertTreeToString());
-	}
+    private String convertTreeToString() {
+        StringBuilder sb = new StringBuilder();
+        treeToSB(classUnderTest.getRoot(), sb);
 
-	private String convertTreeToString() {
-		StringBuilder sb = new StringBuilder();
-		treeToSB(classUnderTest.getRoot(), sb);
+        return sb.toString();
+    }
 
-		return sb.toString();
-	}
+    private void treeToSB(Node node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+        treeToSB(node.left, sb);
 
-	private void treeToSB(Node node, StringBuilder sb) {
-		if (node == null) {
-			return;
-		}
-		treeToSB(node.getLeft(), sb);
+        sb.append(node.val).append(",");
+        Node midNode = node.mid;
+        while (midNode != null) {
+            sb.append(midNode.val).append(",");
+            midNode = midNode.mid;
+        }
 
-		sb.append(node.getVal()).append(",");
-		Node midNode = node.getMid();
-		while (midNode != null) {
-			sb.append(midNode.getVal()).append(",");
-			midNode = midNode.getMid();
-		}
+        treeToSB(node.right, sb);
+    }
 
-		treeToSB(node.getRight(), sb);
-	}
+    private boolean isValidTrinaryTree(Node node, int min, int max) {
+        if (node == null) {
+            return true;
+        }
 
-	private boolean isValidTrinaryTree(Node node, int min, int max) {
-		if (node == null) {
-			return true;
-		}
+        if (node.val > max || node.val < min) {
+            return false;
+        }
 
-		if (node.getVal() > max || node.getVal() < min) {
-			return false;
-		}
+        if (!areAllMidsValid(node)) {
+            return false;
+        }
 
-		if (!areAllMidsValid(node)) {
-			return false;
-		}
+        return isValidTrinaryTree(node.left, min, node.val)
+                && isValidTrinaryTree(node.right, node.val, max);
+    }
 
-		return isValidTrinaryTree(node.getLeft(), min, node.getVal())
-				&& isValidTrinaryTree(node.getRight(), node.getVal(), max);
-	}
+    private boolean areAllMidsValid(Node node) {
+        int val = node.val;
+        Node mid = node.mid;
 
-	private boolean areAllMidsValid(Node node) {
-		int val = node.getVal();
-		Node mid = node.getMid();
+        while (mid != null) {
+            if (mid.val != val || mid.left != null || mid.right != null) {
+                return false;
+            }
+            mid = mid.mid;
+        }
 
-		while (mid != null) {
-			if (mid.getVal() != val || mid.getLeft() != null || mid.getRight() != null) {
-				return false;
-			}
-			mid = mid.getMid();
-		}
-
-		return true;
-	}
+        return true;
+    }
 
 }
