0faabbc2228775da092c9a163fb5a0c172c23abb
John Borries <john.borries@oracle.com>
2022-12-09 18:46:21 -0800

    answered 3 more

--- prep/src/test/java/com/john/Nov2022/leetcode/q101_150/Question101Test.java
+++ prep/src/test/java/com/john/Nov2022/leetcode/q101_150/Question101Test.java
@@ -1,5 +1,6 @@
 package com.john.Nov2022.leetcode.q101_150;
 
+import com.john.datastructures.TreeNode;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,7 +8,20 @@ public class Question101Test {
     private Question101 classUnderTest = new Question101();
 
     @Test
-    public void testUnAttempted() {
-        Assert.fail("unattempted");
+    public void testExample1() {
+
+        TreeNode root = TreeNode.create("[1,2,2,3,4,4,3]");
+        boolean expected = true;
+
+        Assert.assertEquals(expected, classUnderTest.isSymmetric(root));
+    }
+
+    @Test
+    public void testExample2() {
+
+        TreeNode root = TreeNode.create("[1,2,2,null,3,null,3]");
+        boolean expected = false;
+
+        Assert.assertEquals(expected, classUnderTest.isSymmetric(root));
     }
 }
